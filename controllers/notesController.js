2d61b99014d542f8d7f2c8581e40425f2c8dfc9a
nezlioui <idirnezlioui4@gmail.com>
2025-06-05 16:56:07 +0200

    backe de la partie des notes

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -10,4 +10,38 @@ const getModulesByEtudiant = async (req, res) => {
   }
 };
 
-module.exports = { getModulesByEtudiant };
+const getNotesByEtudiant=async(req,res)=>{
+  const{id}=req.params
+
+  try {
+    const notes=await Notes.getNotesByEtudiant(id)
+    res.status(200).json(notes);
+  } catch (error) {
+    res.status(500).json({error:"rreur récupération notes"})
+    
+  }
+}
+
+const insertNotes=async(req,res)=>{
+  const {ref_note, ref_module, num_etudiant, note } = req.body;
+
+  try {
+    await Notes.insertNotes({ ref_note, ref_module, num_etudiant, note })
+     res.status(201).json({ message: 'Note insérée avec succès' });
+  } catch (error) {
+     res.status(500).json({ error: "Erreur lors de l'insertion de la note" });
+  }
+}
+
+const updateNote=async(req,res)=>{
+  const {ref_module, num_etudiant, note } = req.body;
+  try {
+    await Notes.updateNote({ ref_module, num_etudiant, note })
+    res.status(200).json({ message: 'Note mise à jour avec succès' });
+  } catch (error) {
+    res.status(500).json({ error: "Erreur lors de la mise à jour de la note" });
+  }
+}
+
+
+module.exports = { getModulesByEtudiant,getNotesByEtudiant ,insertNotes,updateNote};
--- modls/notes.js
+++ modls/notes.js
@@ -7,6 +7,28 @@ const Notes = {
     );
     return rows;
   },
+
+  //recupère les notes déja enregistre pour un etudiant 
+
+  getNotesByEtudiant:async(num_etudiant)=>{
+    const [rows]=await db.query('SELECT ref_note, ref_module, num_etudiant, note FROM notes WHERE num_etudiant = ?',
+      [num_etudiant])
+    return rows
+  },
+
+  //inseret une nouvelle note
+  insertNotes:async({  ref_module, num_etudiant, note })=>{
+    await db.query('INSERT INTO notes ( ref_module, num_etudiant, note) VALUES ( ?, ?, ?)',
+      [ ref_module, num_etudiant, note])
+  },
+
+  //mis a jours une note existante 
+  updateNote:async({ ref_module, num_etudiant, note })=>{
+    await db.query('UPDATE notes SET note = ? WHERE ref_module = ? AND num_etudiant = ?',
+      [note, ref_module, num_etudiant])
+  }
+
+
 };
 
 module.exports = Notes;
